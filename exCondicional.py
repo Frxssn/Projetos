548edad7302397ac308cd7f0946bb659ee5a5c16
Frxssn <pfrasson72@gmail.com>
2026-05-04 21:30:18 -0300

    Atualização codigo de desconto

---
+++ exCondicional.py
@@ -0,0 +1,30 @@
+valor = float(input("Digite o valor do pedido:"))
+
+"""
+Regra de negócio:
+* Se a venda for até 100 reais, dê 5% de desconto
+* Se a venda for entre 100,01 e 299.99 reais, dê 10% de desconto
+* Se a venda for acima 300, dê 15% de desconto 
+
+"""
+
+if valor <= 100:
+    desconto = 0.95
+   
+elif valor > 100 and valor <= 299.99:
+    desconto = 0.90
+    
+else:
+    desconto = 0.85
+   
+total = valor * desconto
+
+descontoPercentual = (1 - desconto) * 100
+
+descontoPercentual = round(descontoPercentual,0)
+
+print("Valor total foi de:", total, ". Seu desconto foi de:", descontoPercentual, "%")
+
+
+print(f"Sua compra deu R${valor}. Você ganhou {descontoPercentual}% " \
+f"Você ganhou . o total agora é de R${total}.")
